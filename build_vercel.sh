adf82569cd4be66ce7358f2c3c858ded641ad0ae
scentemote-technologies <gowthamwork2003@gmail.com>
2025-12-13 05:08:28 +0530

    Add build script for Vercel deployment
    This script builds the Flutter web application in release mode with necessary environment variables for Firebase and reCAPTCHA.

---
+++ build_vercel.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+flutter build web --release \
+  --dart-define=FIREBASE_API_KEY=$FIREBASE_API_KEY \
+  --dart-define=PLACES_API_KEY=$PLACES_API_KEY \
+  --dart-define=FIREBASE_AUTH_DOMAIN=$FIREBASE_AUTH_DOMAIN \
+  --dart-define=FIREBASE_PROJECT_ID=$FIREBASE_PROJECT_ID \
+  --dart-define=FIREBASE_STORAGE_BUCKET=$FIREBASE_STORAGE_BUCKET \
+  --dart-define=FIREBASE_MESSAGING_SENDER_ID=$FIREBASE_MESSAGING_SENDER_ID \
+  --dart-define=FIREBASE_APP_ID=$FIREBASE_APP_ID \
+  --dart-define=RECAPTCHA_SITE_KEY=$RECAPTCHA_SITE_KEY
